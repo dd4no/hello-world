34c311f66fdabda74ed00377e24986dd488c7256
Dave Defourneaux <dave.defourneaux@gmail.com>
2020-12-15 20:46:31 -0500

    Create binary.py
    Binary Value Reference

---
+++ binary.py
@@ -0,0 +1,14 @@
+index = 0
+total = 0
+while index < 64:
+    value = 2**index
+    total = total + value
+    print("Place ", index, "= ", value, "Running Sum: ", total)
+    index = index + 1
+print()
+
+
+    
+    
+    
+    
