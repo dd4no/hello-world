febd76053bb1088abcee76b55479b494faa4b6ad
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2020-12-18 21:48:48 -0500

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,10 +1,16 @@
 index = 0
 total = 0
+count = 0
+print("PLACE  VALUE          RUNNING SUM")
 while index < 64:
+    count = count + 1
     value = 2**index
     total = total + value
-    print("Place ", index, "= ", value, "Running Sum: ", total)
+    print(index, "     ", value, "           ", total)
     index = index + 1
+    if count == 8:
+        print("----------------------------------------------------------------")
+        count = 0
 print()
 
 
